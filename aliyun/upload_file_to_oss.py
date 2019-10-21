0a30d09dc3fd3d144ca67e5188192d58d3a3414c
hujian <hujian@tigerft.com>
2019-10-21 17:05:09 +0800

    oss完善软链

--- aliyun/upload_file_to_oss.py
+++ aliyun/upload_file_to_oss.py
@@ -5,20 +5,30 @@ import sys
 import os
 
 
-EndPoint = 'oss-cn-hongkong.aliyuncs.com'
+EndPoint = 'oss-cn-shanghai.aliyuncs.com'
 AccessKeyId = ''
 AccessKeySecret = ''
-BucketName = 'viausdfile'
+BucketName = ''
 
 
 def upload_file(oss_dir, f):
     filename = os.path.basename(f)
     ObjectName = oss_dir.strip("/") + "/" + filename
-    #print(filename)
     auth = oss2.Auth(AccessKeyId, AccessKeySecret)
     bucket = oss2.Bucket(auth, EndPoint, BucketName)
     bucket.put_object_from_file(ObjectName, f)
-    print("文件下载地址为:%s" % "https://" + BucketName + "." + EndPoint + oss_dir + filename)
+
+    # 设置文件访问权限公共读
+    bucket.put_object_acl(ObjectName, oss2.OBJECT_ACL_PUBLIC_READ)
+
+    # 软链(软链不能以/开头)
+    bucket.put_symlink(ObjectName, 'apkdownload/innowealth-release.apk')
+
+    # 软链权限
+    bucket.put_object_acl('apkdownload/innowealth-release.apk', oss2.OBJECT_ACL_PUBLIC_READ)
+
+    print("文件下载地址为:%s" % "https://" + BucketName + "." + EndPoint + ObjectName)
+    
 
 
 if __name__ == "__main__":
