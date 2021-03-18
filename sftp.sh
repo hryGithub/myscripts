386cb8a28813adf290bf19d8645bc1aee2c28ef5
hyr <516870171@qq.com>
2021-03-18 17:10:53 +0800

    Create sftp.sh

---
+++ sftp.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USER=
+PASSWORD=
+
+SRCDIR=/application/tempforupdate/
+DESDIR=/onlineshop/DailyFiles/
+
+#IP
+IP=
+
+PORT=22
+
+cd ${SRCDIR}
+FILES=`ls`
+
+
+for FILE in ${FILES};do
+	echo ${FILE}
+	lftp -u ${USER},${PASSWORD} sftp://${IP}:${PORT} <<EOF
+		cd ${DESDIR}
+		lcd ${SRCDIR}
+		put ${FILE}
+		by
+EOF
+done
